e435e847a326a5e437fec10347dae9921ff0d9cf
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-02-23 15:59:10 +0900

    소켓 서버 정의

---
+++ socketserver.py
@@ -0,0 +1,22 @@
+import socket
+
+
+class Socket:
+
+    def __init__(self, ip, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((ip, port))
+
+    def server_listen(self):
+        self.sock.listen(0)
+        print('socket server listen...')
+
+    def server_accept(self):
+        return self.sock.accept()
+
+    @staticmethod
+    def recv_data(client, buff):
+        return client.recv(buff)
+
+    def server_close(self):
+        self.sock.close()
