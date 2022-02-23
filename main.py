720b018a1b6391f313f6c0ee2fd0ba764e1fe098
MoonHongMyeong <moonhongmyeong@gmail.com>
2022-02-23 16:03:03 +0900

    분석 서버 메인

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import struct
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from analyzer.cpuanalyzer import CpuAnalyzer
+from analyzer.memoryanalyzer import MemoryAnalyzer
+from analyzer.storageanalyzer import StorageAnalyzer
 
+from parser.parser import Parser
+from socketserver import Socket
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    IP = '192.168.1.4'
+    PORT = 8000
+    BUFFER = 16
+    PACK_FORMAT = '@hfd'
+
+    CPU_COL_SEC = 1
+    MEM_COL_SEC = 5
+    STR_COL_SEC = 60
+
+    server = Socket(ip=IP, port=PORT)
+    server.server_listen()
+    client, address = server.server_accept()
+    print("client : ", address)
+
+    p = Parser()
+    ca = CpuAnalyzer(avg_sec=60/CPU_COL_SEC,
+                     peak_sec=300/CPU_COL_SEC)
+    ma = MemoryAnalyzer(avg_sec=300/MEM_COL_SEC)
+    sa = StorageAnalyzer(avg_sec=1800/STR_COL_SEC)
+    reset_count = 0
+
+    while 1:
+        recv_data = server.recv_data(client, BUFFER)
+        print(recv_data)
+        unpack_data = struct.unpack(PACK_FORMAT, recv_data)
+        print(unpack_data)
+        if unpack_data[0] == 3:
+            storage_data = p.parse_data(unpack_data)
+            sa.analyze_data(storage_data)
+        elif unpack_data[0] == 2:
+            memory_data = p.parse_data(unpack_data)
+            ma.analyze_data(memory_data)
+        elif unpack_data[0] == 1:
+            cpu_data = p.parse_data(unpack_data)
+            ca.analyze_data(cpu_data)
